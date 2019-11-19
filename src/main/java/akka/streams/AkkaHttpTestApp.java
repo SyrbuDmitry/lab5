8594eb227f3bc73de840efbd533e65f78a6e93b0
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-19 16:48:07 +0300

    Scripted auto-commit on change (2019-11-19 16:48:06) by gitwatch.sh

--- src/main/java/akka/streams/AkkaHttpTestApp.java
+++ src/main/java/akka/streams/AkkaHttpTestApp.java
@@ -1,7 +1,37 @@
 package akka.streams;
 
-public class AkkaHttpTestApp {
-    public static void main(String[] args){
-        System.out.println();
+import akka.NotUsed;
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
+import akka.http.javadsl.ServerBinding;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.javadsl.server.AllDirectives;
+import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
+
+import java.util.concurrent.CompletionStage;
+
+public class AkkaHttpTestApp extends AllDirectives {
+    public static void main(String[] args) throws Exception{
+
+        ActorSystem system = ActorSystem.create("routes");
+        final Http http = Http.get(system);
+        AkkaHttpTestApp instance = new AkkaHttpTestApp();
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
+        final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = instance.createRoute(RouteActor).flow(system, materializer);
+        final CompletionStage<ServerBinding> binding = http.bindAndHandle(
+                routeFlow,
+                ConnectHttp.toHost("localhost", 8085),
+                materializer
+        );
+        System.out.println("Server online at http://localhost:8085/\nPress RETURN to stop...");
+        System.in.read();
+        binding
+                .thenCompose(ServerBinding::unbind)
+                .thenAccept(unbound -> system.terminate());
     }
 }
