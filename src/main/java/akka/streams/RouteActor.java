54429727b3322a5666e5e2c36600f9ef341029e6
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-22 22:13:35 +0300

    Scripted auto-commit on change (2019-11-22 22:13:35) by gitwatch.sh

--- src/main/java/akka/streams/RouteActor.java
+++ src/main/java/akka/streams/RouteActor.java
@@ -1,15 +1,27 @@
 package akka.streams;
 
+import akka.NotUsed;
 import akka.actor.AbstractActor;
 import akka.actor.ActorRef;
 import akka.actor.Props;
+import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
+import akka.http.scaladsl.model.Uri;
 import akka.japi.pf.ReceiveBuilder;
 import akka.routing.RoundRobinPool;
+import akka.stream.javadsl.Flow;
 
 public class RouteActor extends AbstractActor {
     @Override
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .build();
+    }
+    public Flow<HttpRequest,HttpResponse, NotUsed> createRoute(){
+        return Flow.of(HttpResponse.class)
+                .mapConcat(this::parseQuery)
+    }
+    public HttpResponse parseQuery(Uri.Query q){
+        
     }
 }
