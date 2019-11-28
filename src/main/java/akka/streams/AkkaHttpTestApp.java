8f485d05f0d7def40252764fdaf32a0d53da1875
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-28 14:42:12 +0300

    Scripted auto-commit on change (2019-11-28 14:42:12) by gitwatch.sh

--- src/main/java/akka/streams/AkkaHttpTestApp.java
+++ src/main/java/akka/streams/AkkaHttpTestApp.java
@@ -1,23 +1,21 @@
 package akka.streams;
 
 import akka.NotUsed;
-import akka.actor.ActorRef;
+
 import akka.actor.ActorSystem;
-import akka.actor.Props;
+
 import akka.http.javadsl.ConnectHttp;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
-import akka.http.javadsl.marshallers.jackson.Jackson;
+
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.AllDirectives;
-import akka.http.javadsl.server.Route;
-import akka.pattern.Patterns;
+
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.Dsl;
-import scala.concurrent.Future;
+
 
 import java.util.concurrent.CompletionStage;
 
