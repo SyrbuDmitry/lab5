aac426095ba51c5aaa015bf4b2866883ad708d1e
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-28 14:56:51 +0300

    Scripted auto-commit on change (2019-11-28 14:56:51) by gitwatch.sh

--- src/main/java/akka/streams/RouteActor.java
+++ src/main/java/akka/streams/RouteActor.java
@@ -67,7 +67,7 @@ public class RouteActor {
                 .thenCompose(w -> CompletableFuture.completedFuture(
                         Duration.between(startTime,Instant.now()).getSeconds()/r.getCount()
                 ));
-        
+        System.out.println(r.getCount());
         return  whenResponse;
     }
     private HttpResponse convertIntoResponse(Long r){
