c5e29c6ce4deab02e4c16a90f4e5877910b71923
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-29 13:22:46 +0300

    Scripted auto-commit on change (2019-11-29 13:22:46) by gitwatch.sh

--- src/main/java/akka/streams/SaveResultMessage.java
+++ src/main/java/akka/streams/SaveResultMessage.java
@@ -1,4 +1,14 @@
 package akka.streams;
 
 public class SaveResultMessage {
+    private Request request;
+    private Long result;
+
+    public Request getRequest() {
+        return request;
+    }
+
+    public Long getResult() {
+        return result;
+    }
 }
--- src/main/java/akka/streams/StorageActor.java
+++ src/main/java/akka/streams/StorageActor.java
@@ -1,6 +1,7 @@
 package akka.streams;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,4 +9,16 @@ import java.util.Map;
 public class StorageActor extends AbstractActor {
     Map<Request,Long> storage = new HashMap<>();
 
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(SaveResultMessage.class, t->{
+                    storage.put(t.getRequest(),t.getResult());
+                })
+                .match(GetResultMessage.class, msg->{
+                    
+                })
+                .match(GetResultMessage.class, r-> sender())
+                .build();
+    }
 }
