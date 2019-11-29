fbc863dbfc8d424470487dad1ff2c5ac784023d3
SyrbuDmitry <syrbu_dmitry@mail.ru>
2019-11-29 22:37:14 +0300

    Scripted auto-commit on change (2019-11-29 22:37:14) by gitwatch.sh

--- src/main/java/akka/streams/StorageActor.java
+++ src/main/java/akka/streams/StorageActor.java
@@ -14,7 +14,7 @@ public class StorageActor extends AbstractActor {
     public Receive createReceive() {
         return ReceiveBuilder.create()
                 .match(Result.class, t->{
-                    System.out.println(t);
+                    System.out.println(storage);
                     storage.put(t.getRequest(),t.getResult());
                 })
                 .match(Request.class, msg->{
